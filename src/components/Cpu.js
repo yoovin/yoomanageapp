fb0d49f6e50e2e8206e586e62371f711e294de15
yoovin <soo22839@gmail.com>
2021-05-08 01:12:33 +0900

    중간저장

--- src/components/Cpu.js
+++ src/components/Cpu.js
@@ -1,19 +1,15 @@
 import React, {useState, useEffect} from 'react'
-import axios from 'axios'
+
 
 const Cpu = () => {
-  const [cpuInfo, setcpuInfo] = useState('')
+
+  [cpuInfo, setCpuInfo] = useState('')
 
   const fetchData = () => {
-    setInterval(() => {
-      const url = '/cpu'
-      axios.get(url)
-      .then(res=>setcpuInfo(res.data))
-    },2000)
+    
   }
 
   useEffect(()=>{
-    fetchData()
   },[])
 
     return (
